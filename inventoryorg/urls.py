55e2f8152c55d64cd609b5067c78b5be81f48dbd
samkit saraf <samkit09@users.noreply.github.com>
2021-06-08 17:55:12 +0530

    static root

--- inventoryorg/urls.py
+++ inventoryorg/urls.py
@@ -7,23 +7,15 @@ urlpatterns = [
     path('login', views.loginuser, name="login"),
     path('load_serial/',views.load_serial,name="load_serial"),
 
-#     path('adminac/', views.adminac, name="adminac"),
     path('adminac/<int:value>/', views.adminac, name="adminac"),
     path('adminac/<int:value>/<str:pk>/', views.adminac, name="adminac"),
 
-#     path('managerac/', views.managerac, name="managerac"),
     path('managerac/<int:value>/', views.managerac, name="managerac"),
     path('managerac/<int:value>/<str:pk>/', views.managerac, name="managerac"),
 
-#     path('staffac/', views.staffac, name="staffac"),
     path('staffac/<int:value>/', views.staffac, name="staffac"),
     path('staffac/<int:value>/<str:pk>/', views.staffac, name="staffac"),
 
     path('logoutuser/', views.logoutuser, name="logoutuser"),
-#     path('adminac/<int:value>/logoutuser/',
-#          views.logoutuser, name="logoutuser"),
-#     path('managerac/<int:value>/logoutuser/',
-#          views.logoutuser, name="logoutuser"),
-#     path('staffac/<int:value>/logoutuser/',
-#          views.logoutuser, name="logoutuser"),
+
 ]
