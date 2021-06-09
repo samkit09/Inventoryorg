b54aabadc4ee113047da38640ff003813f954857
samkit saraf <samkit09@users.noreply.github.com>
2021-06-09 17:04:05 +0530

    Update settings.py

--- dproject/settings.py
+++ dproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-luz07tmn96cpau=acqju1*#^kdz)j$lt@(0=^brc9w4w*s^hzs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'inventoryorg.herokuapp.com']
 
