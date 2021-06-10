d70b7a0e05d3796cd5a14435b469471aeff1e7c7
samkit saraf <samkit09@users.noreply.github.com>
2021-06-10 13:50:19 +0530

    deb

--- dproject/settings.py
+++ dproject/settings.py
@@ -42,8 +42,8 @@ LOGGING = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = False
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '.inventoryorg.herokuapp.com',
                  'https://inventoryorg.herokuapp.com', 'www.inventoryorg.herokuapp.com']
